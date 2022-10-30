576c676be48aac1e1c57b124187769381401f2e8
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2022-10-30 23:49:09 +0100

    mise a jour du trait de reponse d'API
    prise en compte des messages par defaut
    lorsqu'on envoir directement les donnees comme premier parametre

--- ApiResponseTrait.php
+++ ApiResponseTrait.php
@@ -92,71 +92,89 @@ trait ApiResponseTrait
     /**
      * Reponse de type bad request
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondBadRequest(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondBadRequest($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['invalid_request'] ?? StatusCode::BAD_REQUEST, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Bad Request', $this->codes['invalid_request'] ?? StatusCode::BAD_REQUEST, $code, $errors);
     }
 
     /**
      * À utiliser lorsque vous essayez de créer une nouvelle ressource et qu'elle existe déjà.
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondConflict(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondConflict($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['conflict'] ?? StatusCode::CONFLICT, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Conflict', $this->codes['conflict'] ?? StatusCode::CONFLICT, $code, $errors);
     }
 
     /**
      * Utilisé après la création réussie d'une nouvelle ressource.
      *
      * @param mixed|null $result
+     * @param mixed      $message
      *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondCreated(string $message, $result = null)
+    final protected function respondCreated($message, $result = null)
     {
-        return $this->respondSuccess($message, $result, $this->codes['created'] ?? StatusCode::CREATED);
+        ['message' => $message, 'data' => $result] = $this->_parseParams($message, null, $result);
+
+        return $this->respondSuccess($message ?? 'Created', $result, $this->codes['created'] ?? StatusCode::CREATED);
     }
 
     /**
      * Utilisé après qu'une ressource a été supprimée avec succès.
      *
      * @param mixed|null $result
+     * @param mixed      $message
      *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    protected function respondDeleted(string $message, $result = null)
+    protected function respondDeleted($message, $result = null)
     {
-        return $this->respondSuccess($message, $result, $this->codes['deleted'] ?? StatusCode::OK);
+        ['message' => $message, 'data' => $result] = $this->_parseParams($message, null, $result);
+
+        return $this->respondSuccess($message ?? 'Deleted', $result, $this->codes['deleted'] ?? StatusCode::OK);
     }
 
     /**
      * Utilisé lorsque l'accès est toujours refusé à cette ressource
      * et qu'aucune nouvelle tentative n'aidera.
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondForbidden(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondForbidden($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['forbidden'] ?? StatusCode::FORBIDDEN, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Forbidden', $this->codes['forbidden'] ?? StatusCode::FORBIDDEN, $code, $errors);
     }
 
     /**
@@ -164,61 +182,77 @@ trait ApiResponseTrait
      * car ici, nous savons que les données existaient auparavant, mais sont maintenant disparues,
      * où Not Found signifie que nous ne pouvons tout simplement pas trouver d'informations à leur sujet.
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondGone(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondGone($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['resource_gone'] ?? StatusCode::GONE, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Gone', $this->codes['resource_gone'] ?? StatusCode::GONE, $code, $errors);
     }
 
     /**
      * Utilisé lorsqu'il y a une erreur de serveur.
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondInternalError(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondInternalError($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['server_error'] ?? StatusCode::INTERNAL_ERROR, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Internal Server Error', $this->codes['server_error'] ?? StatusCode::INTERNAL_ERROR, $code, $errors);
     }
 
     /**
      * Reponse de type invalid token
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondInvalidToken(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondInvalidToken($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['invalid_token'] ?? StatusCode::INVALID_TOKEN, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Invalid Token', $this->codes['invalid_token'] ?? StatusCode::INVALID_TOKEN, $code, $errors);
     }
 
     /**
      * Reponse de type method not allowed
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondMethodNotAllowed(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondMethodNotAllowed($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['not_allowed'] ?? StatusCode::METHOD_NOT_ALLOWED, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Method Not Allowed', $this->codes['not_allowed'] ?? StatusCode::METHOD_NOT_ALLOWED, $code, $errors);
     }
 
     /**
@@ -227,7 +261,7 @@ trait ApiResponseTrait
      *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondNoContent(string $message)
+    final protected function respondNoContent(string $message = 'No Content')
     {
         return $this->respondSuccess($message, null, $this->codes['no_content'] ?? StatusCode::NO_CONTENT);
     }
@@ -235,73 +269,92 @@ trait ApiResponseTrait
     /**
      * Reponse de type not acceptable
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondNotAcceptable(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondNotAcceptable($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['not_acceptable'] ?? StatusCode::NOT_ACCEPTABLE, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Not Acceptable', $this->codes['not_acceptable'] ?? StatusCode::NOT_ACCEPTABLE, $code, $errors);
     }
 
     /**
      * Utilisé lorsqu'une ressource spécifiée est introuvable.
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondNotFound(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondNotFound($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['resource_not_found'] ?? StatusCode::NOT_FOUND, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Not Found', $this->codes['resource_not_found'] ?? StatusCode::NOT_FOUND, $code, $errors);
     }
 
     /**
      * Reponse de type not implemented
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondNotImplemented(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondNotImplemented($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['not_implemented'] ?? StatusCode::NOT_IMPLEMENTED, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Not Implemented', $this->codes['not_implemented'] ?? StatusCode::NOT_IMPLEMENTED, $code, $errors);
     }
 
     /**
      * Reponse de type ok
      *
      * @param mixed|null $result
+     * @param mixed      $message
      *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondOk(string $message, $result = null)
+    final protected function respondOk($message, $result = null)
     {
-        return $this->respondSuccess($message, $result, $this->codes['ok'] ?? StatusCode::OK);
+        ['message' => $message, 'data' => $result] = $this->_parseParams($message, null, $result);
+
+        return $this->respondSuccess($message ?? 'Ok', $result, $this->codes['ok'] ?? StatusCode::OK);
     }
 
     /**
      * Utilisé lorsque l'utilisateur a fait trop de demandes pour la ressource récemment.
      *
-     * @return \Psr\Http\Message\ResponseInterface|void
+     * @param mixed $message
+     *
+     * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondTooManyRequests(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondTooManyRequests($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['too_many_requests'] ?? StatusCode::TOO_MANY_REQUESTS, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Too Many Requests', $this->codes['too_many_requests'] ?? StatusCode::TOO_MANY_REQUESTS, $code, $errors);
     }
 
     /**
@@ -309,27 +362,61 @@ trait ApiResponseTrait
      * ou avait de mauvaises informations d'identification d'autorisation.
      * L'utilisateur est encouragé à réessayer avec les informations appropriées.
      *
+     * @param mixed $message
+     *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondUnauthorized(string $message, int|string|null|array $code = null, array $errors = [])
+    final protected function respondUnauthorized($message, int|string|null|array $code = null, array $errors = [])
     {
         if (is_array($code)) {
             $errors = $code;
             $code   = null;
         }
 
-        return $this->respondFail($message, $this->codes['unauthorized'] ?? StatusCode::UNAUTHORIZED, $code, $errors);
+        ['message' => $message, 'data' => $errors, 'code' => $code] = $this->_parseParams($message, $code, $errors);
+
+        return $this->respondFail($message ?? 'Unauthorized', $this->codes['unauthorized'] ?? StatusCode::UNAUTHORIZED, $code, $errors);
     }
 
     /**
      * Utilisé après qu'une ressource a été mise à jour avec succès.
      *
      * @param mixed|null $result
+     * @param mixed      $message
      *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    final protected function respondUpdated(string $message, $result = null)
+    final protected function respondUpdated($message, $result = null)
     {
-        return $this->respondSuccess($message, $result, $this->codes['updated'] ?? StatusCode::OK);
+        ['message' => $message, 'data' => $result] = $this->_parseParams($message, null, $result);
+
+        return $this->respondSuccess($message ?? 'Updated', $result, $this->codes['updated'] ?? StatusCode::OK);
+    }
+
+    /**
+     * Parse les parametres a renvoyer comme corps de la reponse json
+     *
+     * @param array|object|string $message
+     * @param array|int|string    $code
+     * @param array|mixed         $data
+     *
+     * @internal
+     */
+    private function _parseParams($message, $code = null, $data = []): array
+    {
+        if (is_array($message) || is_object($message)) {
+            if (empty($data)) {
+                $data = $message;
+            }
+            $message = null;
+        }
+        if (is_array($code)) {
+            if (empty($data)) {
+                $data = $code;
+            }
+            $code = null;
+        }
+
+        return compact('message', 'code', 'data');
     }
 }
