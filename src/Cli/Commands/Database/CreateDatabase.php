46dc8a2672b5d5bb13822d86347dde3ee4bf028c
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-03-17 11:20:25 +0100

    feat: commande pour la creation de bd

---
+++ src/Cli/Commands/Database/CreateDatabase.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace BlitzPHP\Cli\Commands\Database;
+
+use BlitzPHP\Cli\Console\Command;
+use BlitzPHP\Config\Database;
+use InvalidArgumentException;
+
+class CreateDatabase extends Command
+{
+    /**
+     * @var string Groupe
+     */
+    protected $group = 'Database';
+
+    /**
+     * @var string Nom
+     */
+    protected $name = 'db:create';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $description = 'Créez un nouveau schéma de base de données.';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $service = 'Service de gestion de base de données';
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected $arguments = [
+        'name' => 'Le nom de la base de données à utiliser',
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $options = [
+        '--ext' => 'Extension de fichier du fichier de base de données pour SQLite3. Peut être `db` ou `sqlite`. La valeur par défaut est `db`.',
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function execute(array $params)
+    {
+        $name = $this->argument('name');
+        if (empty($name)) {
+            $name = $this->prompt('Nom de la base de données', null, function($val) {
+                if (empty($val)) {
+                    throw new InvalidArgumentException('Veuillez entrer le nom de la base de données.');
+                }
+            });
+        }
+
+        [$group, $config] = Database::connectionInfo();
+
+        $config['database'] = '';
+        $config['debug']    = false;
+
+        $db = Database::connect($config);
+
+        // Specialement pour SQLite3
+        /*
+        if ($db instanceof Connection) {
+            $ext = $this->option('ext', 'db');
+
+            if (! in_array($ext, ['db', 'sqlite'], true)) {
+                $ext = $this->prompt('Please choose a valid file extension', ['db', 'sqlite']); // @codeCoverageIgnore
+            }
+
+            if ($name !== ':memory:') {
+                $name = str_replace(['.db', '.sqlite'], '', $name) . ".{$ext}";
+            }
+
+            $config['driver']   = 'SQLite3';
+            $config['database'] = $name;
+
+            if ($name !== ':memory:') {
+                $dbName = strpos($name, DIRECTORY_SEPARATOR) === false ? RESOURCE_PATH . $name : $name;
+
+                if (is_file($dbName)) {
+                    $this->error("La base de données \"{$dbName}\" existe déjà.");
+
+                    return;
+                }
+
+                unset($dbName);
+            }
+
+            // Connection a un nouveau SQLite3 pour creer la bd
+            $db = Database::connect($config, false);
+            $db->connect();
+
+            if (! is_file($db->getDatabase()) && $name !== ':memory:') {
+                // @codeCoverageIgnoreStart
+                $this->error('Echec de la création de la base de données');
+
+                return;
+                // @codeCoverageIgnoreEnd
+            }
+        } else */ 
+        if (! Database::creator($db)->createDatabase($name)) {
+            // @codeCoverageIgnoreStart
+            $this->error('Echec de la création de la base de données');
+
+            return;
+            // @codeCoverageIgnoreEnd
+        }
+
+        $this->success("Base de données \"{$name}\" créée avec succès.");    
+    }
+}
