47e8d9878160cbc143fb21766ba613597c0bf6fb
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-04-12 17:28:51 +0200

    feat: Deplacement du helper Fluent + ajout de Invade

--- Support/Fluent.php
+++ Support/Fluent.php
@@ -9,7 +9,7 @@
  * the LICENSE file that was distributed with this source code.
  */
 
-namespace BlitzPHP\Utilities;
+namespace BlitzPHP\Utilities\Support;
 
 use ArrayAccess;
 use BlitzPHP\Contracts\Support\Arrayable;
