a4459e4e8221721c22f3ce589c56ccb18ae07d2a
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-04-21 11:43:04 +0200

    feat: session interface

--- Session/CookieInterface.php
+++ Session/CookieInterface.php
@@ -9,7 +9,7 @@
  * the LICENSE file that was distributed with this source code.
  */
 
-namespace BlitzPHP\Contracts\Http;
+namespace BlitzPHP\Contracts\Session;
 
 use DateTime;
 use DateTimeImmutable;
