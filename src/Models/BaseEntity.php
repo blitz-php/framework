3f2c5e3ba17ae353e8d70fbddbbbb3b6aeb96a85
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-05-28 09:09:34 +0200

    feat: BaseEntity

---
+++ src/Models/BaseEntity.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BlitzPHP\Models;
+
+use BlitzPHP\Wolke\Model;
+
+abstract class BaseEntity extends Model
+{
+    
+}
