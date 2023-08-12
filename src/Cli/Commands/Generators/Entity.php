4193052a9847e7fb2d0e5fcf010c424fd8f711a4
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-08-12 10:23:17 +0200

    feat: Generateur d'entites

---
+++ src/Cli/Commands/Generators/Entity.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BlitzPHP\Cli\Commands\Generators;
+
+use BlitzPHP\Cli\Console\Command;
+use BlitzPHP\Cli\Traits\GeneratorTrait;
+
+/**
+ * Génère un fichier squelette d'entité.
+ */
+class Entity extends Command
+{
+    use GeneratorTrait;
+
+    /**
+     * @var string Groupe
+     */
+    protected $group = 'Generateurs';
+
+    /**
+     * @var string Nom
+     */
+    protected $name = 'make:entity';
+
+    /**
+     * @var string Description
+     */
+    protected $description = 'Génère un nouveau fichier d\'entité.';
+
+    /**
+     * @var array Arguments
+     */
+    protected $arguments = [
+        'name' => 'Le nom de la classe d\'entité.',
+    ];
+
+    /**
+     * @var array Options
+     */
+    protected $options = [
+        '--namespace' => ['Définit l\'espace de noms racine. Par défaut : "APP_NAMESPACE".', APP_NAMESPACE],
+        '--suffix'    => 'Ajouter le titre du composant au nom de la classe (par exemple, User => UserEntity).',
+        '--force'     => 'Forcer à écraser le fichier existant.',
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function execute(array $params)
+    {
+        $this->component = 'Entity';
+        $this->directory = 'Entities';
+        $this->template  = 'entity.tpl.php';
+
+        $this->classNameLang = 'CLI.generator.className.entity';
+        $this->runGeneration($params);
+    }
+}
---
+++ src/Cli/Commands/Generators/Views/entity.tpl.php
@@ -0,0 +1,11 @@
+<@php
+
+namespace {namespace};
+
+use BlitzPHP\Models\BaseEntity;
+
+class {class} extends BaseEntity
+{
+    protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
+    protected $casts   = [];
+}
