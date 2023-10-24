0b7c6fbdd69dcd39e1a58b235fb971d3ab5b7186
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-10-24 20:29:13 +0200

    test: commande de generation de cle

---
+++ spec/system/framework/Cli/Commands/GenerateKey.spec.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use BlitzPHP\Spec\CliOutputHelper as COH;
+use BlitzPHP\Spec\StreamInterceptor;
+
+function resetEnvironment(): void
+{
+    putenv('encryption.key');
+    unset($_ENV['encryption.key'], $_SERVER['encryption.key']);
+}
+
+describe('Commandes / GenerateKey', function () {
+    beforeEach(function() {
+        COH::setUp();
+
+        $this->envPath       = ROOTPATH . '.env';
+        $this->backupEnvPath = ROOTPATH . '.env.backup';
+
+        if (is_file($this->envPath)) {
+            rename($this->envPath, $this->backupEnvPath);
+        }
+        
+        resetEnvironment();
+    });
+
+    afterEach(function() {
+        COH::tearDown();
+
+        if (is_file($this->envPath)) {
+            unlink($this->envPath);
+        }
+
+        if (is_file($this->backupEnvPath)) {
+            rename($this->backupEnvPath, $this->envPath);
+        }
+
+        resetEnvironment();
+    });
+
+    beforeAll(function() {
+        COH::setUpBeforeClass();
+    });
+
+    afterAll(function() {
+        COH::tearDownAfterClass();
+    });
+    
+    it(': GenerateKey affiche la clé codée', function () {
+        command('key:generate --show');
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'hex2bin:'));
+        
+        command('key:generate --prefix=base64 --show');
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'base64:'));
+        
+        command('key:generate --prefix=hex2bin --show');
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'hex2bin:'));
+    });
+
+    it(': GenerateKey génère une nouvelle la clé', function () {
+        command('key:generate');
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'SUCCESS'));
+        expect(file_get_contents($this->envPath))->toMatch(fn($actual) => str_contains($actual, env('encryption.key')));
+        expect(file_get_contents($this->envPath))->toMatch(fn($actual) => str_contains($actual, 'hex2bin:'));
+       
+        command('key:generate --prefix=base64 --force');
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'SUCCESS'));    
+        // expect(file_get_contents($this->envPath))->toMatch(fn($actual) => str_contains($actual, env('encryption.key')));
+        expect(file_get_contents($this->envPath))->toMatch(fn($actual) => str_contains($actual, 'base64:'));
+        
+        command('key:generate --prefix=hex2bin --force');
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'SUCCESS'));      
+        // expect(file_get_contents($this->envPath))->toMatch(fn($actual) => str_contains($actual, env('encryption.key')));
+        expect(file_get_contents($this->envPath))->toMatch(fn($actual) => str_contains($actual, 'hex2bin:'));
+    });
+
+    it(": Le fichier .env.example n'existe pas", function () {
+        rename(ROOTPATH . '.env.example', ROOTPATH . 'lostenv');
+        command('key:generate');
+        rename(ROOTPATH . 'lostenv', ROOTPATH . '.env.example');
+        
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'livré par défaut'));
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'Erreur dans la configuration'));
+    });
+
+    it(": Le fichier .env existe mais il est vide", function () {
+        file_put_contents($this->envPath, '');
+        
+        command('key:generate');
+        
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'SUCCESS'));
+        expect(file_get_contents($this->envPath))->toBe("\nencryption.key = " . env('encryption.key'));
+    });
+
+    it(": Clé générée lorsque la nouvelle clé hexadécimale est ensuite commentée", function () {
+        command('key:generate');
+        $key = env('encryption.key', '');
+        
+        file_put_contents($this->envPath, str_replace(
+            'encryption.key = ' . $key,
+            '# encryption.key = ' . $key,
+            file_get_contents($this->envPath),
+            $count
+        ));
+
+        expect($count)->toBe(1); // Échec de la mise en commentaire de la clé précédemment définie.
+        
+        StreamInterceptor::$buffer = '';
+        command('key:generate --force');
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'SUCCESS'));
+        // expect($key)->not->toBe(env('encryption.key', $key)); // Échec du remplacement de la clé commentée.
+    });
+
+    it(": Clé générée lorsque la nouvelle clé base64 est ensuite commentée", function () {
+        command('key:generate --prefix=base64');
+        $key = env('encryption.key', '');
+        
+        file_put_contents($this->envPath, str_replace(
+            'encryption.key = ' . $key,
+            '# encryption.key = ' . $key,
+            file_get_contents($this->envPath),
+            $count
+        ));
+
+        expect($count)->toBe(1); // Échec de la mise en commentaire de la clé précédemment définie.
+        
+        StreamInterceptor::$buffer = '';
+        command('key:generate --force');
+        expect(COH::buffer())->toMatch(fn($actual) => str_contains($actual, 'SUCCESS'));
+        // expect($key)->not->toBe(env('encryption.key', $key)); // Échec du remplacement de la clé commentée.
+    });
+});
---
+++ src/Spec/CliOutputHelper.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BlitzPHP\Spec;
+
+use BlitzPHP\Facades\Fs;
+use php_user_filter;
+use ReturnTypeWillChange;
+
+use const STDERR;
+use const STDOUT;
+
+/**
+ * Pour tester la sortie de la console.
+ */
+class CliOutputHelper
+{
+    protected static $ou = TEMP_PATH . 'output.test';
+
+    public static function setUpBeforeClass(): void
+    {
+        if ( ! is_dir($dirname = pathinfo(static::$ou, PATHINFO_DIRNAME))) {
+            mkdir($dirname, 0777, true);
+        }
+
+        // Thanks: https://stackoverflow.com/a/39785995
+        stream_filter_register('intercept', StreamInterceptor::class);
+        stream_filter_append(STDOUT, 'intercept');
+        stream_filter_append(STDERR, 'intercept');
+    }
+
+    public static function setUp(): void
+    {
+        ob_start();
+        StreamInterceptor::$buffer = '';
+        file_put_contents(static::$ou, '', LOCK_EX);
+    }
+
+    public static function tearDown(): void
+    {
+        ob_end_clean();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        // Make sure we clean up after ourselves:
+        if (is_dir($dirname = pathinfo(static::$ou, PATHINFO_DIRNAME))) {
+            Fs::deleteDirectories($dirname);
+        }
+    }
+
+    public static function buffer()
+    {
+        return StreamInterceptor::$buffer ?: file_get_contents(static::$ou);
+    }
+}
+
+class StreamInterceptor extends php_user_filter
+{
+    public static $buffer = '';
+
+    #[ReturnTypeWillChange]
+    public function filter($in, $out, &$consumed, $closing): int
+    {
+        while ($bucket = stream_bucket_make_writeable($in)) {
+            static::$buffer .= $bucket->data;
+        }
+
+        return PSFS_PASS_ON;
+    }
+}
