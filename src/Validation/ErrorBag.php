d1257571b9b11c5342ce57efc97e5a50b8cb4cda
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-01-03 18:38:35 +0100

    Mettre à jour dimtrovich/validation à la version 1.3
    S'assurer que le ErrorBag est vide en vérifiant s'il y a des erreurs présentes.

--- src/Validation/ErrorBag.php
+++ src/Validation/ErrorBag.php
@@ -16,78 +16,10 @@ use Dimtrovich\Validation\ErrorBag as DimtrovichErrorBag;
 class ErrorBag extends DimtrovichErrorBag
 {
     /**
-     * Obtenir des messages à partir d'une clé donnée, peut être utilisé dans un format personnalisé
+     * Verifie s'il n'y a aucune erreur dans le bag a erreur
      */
-    public function get(string $key, string $format = ':message'): array
+    public function empty(): bool
     {
-        [$key, $ruleName] = $this->parsekey($key);
-        $results          = [];
-        if ($this->isWildcardKey($key)) {
-            $messages = $this->filterMessagesForWildcardKey($key, $ruleName);
-
-            foreach ($messages as $explicitKey => $keyMessages) {
-                foreach ($keyMessages as $rule => $message) {
-                    $results[$explicitKey][$rule] = $this->formatMessage($message, $format);
-                }
-            }
-        } else {
-            $keyMessages = $this->messages[$key] ?? [];
-
-            foreach ((array) $keyMessages as $rule => $message) {
-                if ($ruleName && $ruleName !== $rule) {
-                    continue;
-                }
-                $results[$rule] = $this->formatMessage($message, $format);
-            }
-        }
-
-        return $results;
-    }
-
-    /**
-     * Obtenir tous les messages
-     */
-    public function all(string $format = ':message'): array
-    {
-        $messages = $this->messages;
-
-        $results = [];
-
-        foreach ($messages as $key => $keyMessages) {
-            foreach ((array) $keyMessages as $message) {
-                $results[] = $this->formatMessage($message, $format);
-            }
-        }
-
-        return $results;
-    }
-
-    /**
-     * Filtrer les messages avec une clé générique
-     *
-     * @param mixed|null $ruleName
-     */
-    protected function filterMessagesForWildcardKey(string $key, $ruleName = null): array
-    {
-        $messages = $this->messages;
-        $pattern  = preg_quote($key, '#');
-        $pattern  = str_replace('\*', '.*', $pattern);
-
-        $filteredMessages = [];
-
-        foreach ($messages as $k => $keyMessages) {
-            if ((bool) preg_match('#^' . $pattern . '\z#u', $k) === false) {
-                continue;
-            }
-
-            foreach ((array) $keyMessages as $rule => $message) {
-                if ($ruleName && $rule !== $ruleName) {
-                    continue;
-                }
-                $filteredMessages[$k][$rule] = $message;
-            }
-        }
-
-        return $filteredMessages;
+        return $this->count() == 0;
     }
 }
