e95f8bc99c638b103956348843b71404a55e0b53
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-01-03 18:45:29 +0100

    feat : Introduction du middleware ShareErrorsFromSession
    Cette modification introduit un nouvel middleware appelé ShareErrorsFromSession.
    Il est responsable du partage de la variable de session "errors" avec toutes les instances de vue, ce qui rend les données d'erreur facilement accessibles sans qu'il soit nécessaire de les lier explicitement.
    
    Le middleware crée un sac d'erreurs vide lorsqu'aucune erreur n'est présente.
    Cette disponibilité constante des données d'erreur permet aux développeurs de supposer en toute confiance la présence d'erreurs sans avoir à les vérifier continuellement.
    
    Cet ajout améliore la lisibilité du code et réduit le code " boilerplate " en centralisant la logique de partage des erreurs.
    Il résout le problème de la diffusion efficace des données d'erreur dans plusieurs vues.
    
    Note : Le code existant dans src/Http/Redirection.php a été modifié pour supprimer le partage explicite des erreurs et s'appuie à la place sur le nouveau middleware pour le partage des erreurs.

---
+++ src/Middlewares/ShareErrorsFromSession.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BlitzPHP\Middlewares;
+
+use BlitzPHP\Validation\ErrorBag;
+use BlitzPHP\View\View;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ShareErrorsFromSession implements MiddlewareInterface
+{
+    /**
+     * Create a new error binder instance.
+     */
+    public function __construct(protected View $view)
+    {
+    }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @param \BlitzPHP\Http\Request $request
+	 */
+	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+	{
+		// Si la session courante a une variable "errors" liée à elle,
+		// nous partagerons sa valeur avec toutes les instances de vue afin que les vues puissent facilement accéder aux erreurs sans avoir à se lier.
+		// Un sac vide est défini lorsqu'il n'y a pas d'erreurs.
+        $this->view->share(
+            'errors',  new ErrorBag($request->session()->getFlashdata('errors') ?: [])
+        );
+
+        // Le fait de placer les erreurs dans la vue pour chaque vue permet au développeur de supposer que certaines erreurs sont toujours disponibles,
+		// ce qui est pratique puisqu'il n'a pas à vérifier continuellement la présence d'erreurs.
+
+        return $handler->handle($request);
+    }
+}
