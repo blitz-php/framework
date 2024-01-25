77bfc9c42c694d9bd2f79cb37c48329209ced332
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-01-25 14:01:19 +0100

    fix: withErrors
    la methode withErrors defini une variable partagee. Or s'il y'a deja eu un addData, il ne sera pas disponible a l'adapter

--- src/View/View.php
+++ src/View/View.php
@@ -241,6 +241,10 @@ class View implements Stringable
             $key = [$key => $value];
         }
 
+		if (isset($key['errors'])) {
+			$this->withErrors($key['errors']);
+		}
+
         return $this->addData($key, $context);
     }
 
@@ -264,7 +268,7 @@ class View implements Stringable
             $errors = new ErrorBag($messages);
         }
 
-        $this->share('errors', $errors);
+		$this->adapter->setVar('errors', $errors);
 
         return $this;
     }
