a435ea101e1e9ac64ee1e142b8069a9fe97c66a5
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-03-31 19:14:14 +0200

    test: cache spec

---
+++ spec/system/cache/Cache.spec.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use BlitzPHP\Cache\Cache;
+use BlitzPHP\Cache\Handlers\Dummy;
+use BlitzPHP\Cache\InvalidArgumentException;
+use BlitzPHP\Spec\ReflectionHelper;
+
+describe('Cache / Cache Factory', function () {
+
+	describe('Erreur de gestionnaire de cache', function () {
+		it('Renvoie Dummy lorsque le cache est désactivé', function () {
+			$cache = new Cache();
+			Cache::disable();
+
+			$factory = ReflectionHelper::getPrivateMethodInvoker($cache, 'factory');
+
+			expect(call_user_func($factory))->toBeAnInstanceOf(Dummy::class);
+
+			Cache::enable();
+		});
+
+		it('Leve une exception lorsque les gestionnaires valides ne sont pas definis', function () {
+			$cache = new Cache();
+
+			expect(ReflectionHelper::getPrivateProperty($cache, 'config'))->toBe([]);
+
+			$factory = ReflectionHelper::getPrivateMethodInvoker($cache, 'factory');
+
+			expect(fn() => call_user_func($factory))->toThrow(new InvalidArgumentException());
+		});
+
+		it('Leve une exception lorsque le gestionnaire principal n\'est pas defini ou ne fait pas partir des gestionnaires valides', function () {
+			$config                  = [];
+			$config['valid_handlers'] = config('cache.valid_handlers');
+			$cache                   = new Cache($config);
+
+			$factory = ReflectionHelper::getPrivateMethodInvoker($cache, 'factory');
+			expect(fn() => call_user_func($factory))->toThrow(new InvalidArgumentException());
+
+
+			$config['handler'] = 'fake_handler';
+			$cache             = new Cache($config);
+
+			$factory = ReflectionHelper::getPrivateMethodInvoker($cache, 'factory');
+			expect(fn() => call_user_func($factory))->toThrow(new InvalidArgumentException());
+		});
+
+		it('Utilise Dummy si le gestionnaire principal fait partir des gestionnaires valides mais n\'herite pas de BaseHandler', function () {
+			$config                   = [];
+			$config['valid_handlers'] = ['fake_handler' => stdClass::class];
+			$config['handler']        = 'fake_handler';
+			$cache                    = new Cache($config);
+
+			$factory = ReflectionHelper::getPrivateMethodInvoker($cache, 'factory');
+			expect(call_user_func($factory))->toBeAnInstanceOf(Dummy::class);
+		});
+	});
+});
