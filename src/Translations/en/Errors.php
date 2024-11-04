78d0e5d94819344015117df287dcb4f0fb4eca8c
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-11-04 16:34:18 +0100

    feat: gestion des pages d'erreur en production

---
+++ src/Translations/en/Errors.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Errors language settings
+return [
+    'pageNotFound'    => '404 - Page Not Found',
+    'sorryCannotFind' => 'Sorry! Cannot seem to find the page you were looking for.',
+    'badRequest'      => '400 - Bad Request',
+    'sorryBadRequest' => 'Sorry! Something is wrong with your request.',
+    'whoops'          => 'Whoops!',
+    'weHitASnag'      => 'We seem to have hit a snag. Please try again later...',
+];
