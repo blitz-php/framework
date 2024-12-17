1d6290403470f525999b022ebc4cec1334a57756
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-12-17 14:46:36 +0100

    fix: correction du formattage XML via XmlFormatter

--- src/Formatter/XmlFormatter.php
+++ src/Formatter/XmlFormatter.php
@@ -11,6 +11,7 @@
 
 namespace BlitzPHP\Formatter;
 
+use BlitzPHP\Exceptions\FormatException;
 use SimpleXMLElement;
 
 /**
@@ -18,15 +19,15 @@ use SimpleXMLElement;
  */
 class XmlFormatter implements FormatterInterface
 {
-    /**
-     * @var string
-     */
-    protected $basenode = 'xml';
+    public function __construct()
+    {
+        // SimpleXML est installé par défaut, mais il est préférable de vérifier, puis de fournir une solution de repli.
+        if (! extension_loaded('simplexml')) {
+            throw FormatException::missingExtension(); // @codeCoverageIgnore
+        }
 
-    /**
-     * @var SimpleXMLElement
-     */
-    protected $structure;
+        helper('inflector');
+    }
 
     /**
      * {@inheritDoc}
@@ -36,15 +37,37 @@ class XmlFormatter implements FormatterInterface
      */
     public function format($data)
     {
-        if (empty($this->structure)) {
-            $this->structure = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><{$this->basenode} />");
+        $basenode  = 'response';
+        $structure = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><{$basenode} />");
+
+        $this->arrayToXml((array) $data, $structure, $basenode);
+
+        return $structure->asXML();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param string $data Chaine XML
+     */
+    public function parse(string $data): array
+    {
+        $xml = simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOCDATA);
+
+        if ($xml === false) {
+            return [];
         }
 
-        // Forcez-le à être quelque chose d'utile
-        if (! is_array($data) && ! is_object($data)) {
-            $data = (array) $data;
-        }
+        $json = json_encode($xml);
 
+        return json_decode($json, true);
+    }
+
+    /**
+     * Une méthode récursive pour convertir un tableau en une chaîne XML valide.
+     */
+    protected function arrayToXml(array $data, SimpleXMLElement &$structure, string $basenode): void
+    {
         foreach ($data as $key => $value) {
             // change false/true en 0/1
             if (is_bool($value)) {
@@ -53,9 +76,8 @@ class XmlFormatter implements FormatterInterface
 
             // pas de touches numériques dans notre xml s'il vous plait !
             if (is_numeric($key)) {
-                helper('inflector');
                 // crée une clé de chaîne...
-                $key = singular($this->basenode) !== $this->basenode ? singular($this->basenode) : 'item';
+                $key = singular($basenode) !== $basenode ? singular($basenode) : 'item';
             }
 
             // remplace tout ce qui n'est pas alphanumérique
@@ -68,35 +90,21 @@ class XmlFormatter implements FormatterInterface
                 }
 
                 foreach ($attributes as $attribute_name => $attribute_value) {
-                    $this->structure->addAttribute($attribute_name, $attribute_value);
+                    $structure->addAttribute($attribute_name, $attribute_value);
                 }
             }
             // s'il y a un autre tableau trouvé appelez récursivement cette fonction
             elseif (is_array($value) || is_object($value)) {
-                $node = $this->structure->addChild($key);
+                $node = $structure->addChild($key);
 
                 // appel récursif
-                $this->structure = $node;
-                $this->basenode  = $key;
-                $this->format($value);
+                $this->arrayToXml($value, $node, $key);
             } else {
                 // ajouter un seul noeud
-                $value = htmlspecialchars(html_entity_decode($value, ENT_QUOTES, 'UTF-8'), ENT_QUOTES, 'UTF-8');
+                $value = htmlspecialchars(html_entity_decode($value ?? '', ENT_QUOTES, 'UTF-8'), ENT_QUOTES, 'UTF-8');
 
-                $this->structure->addChild($key, $value);
+                $structure->addChild($key, $value);
             }
         }
-
-        return $this->structure->asXML();
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param string $data Chaine XML
-     */
-    public function parse(string $data): array
-    {
-        return $data !== '' && $data !== '0' ? (array) simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOCDATA) : [];
     }
 }
