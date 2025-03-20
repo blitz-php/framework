e1b15a30d9622ca02255616bf3fc5b434666a298
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2025-03-20 17:25:40 +0100

    feat: ajout du hot reload

--- spec/support/application/app/Config/toolbar.php
+++ spec/support/application/app/Config/toolbar.php
@@ -10,10 +10,12 @@
  */
 
 return [
-    'collectors'       => [],
-    'collect_var_data' => true,
-    'max_history'      => 20,
-    'view_path'        => SYST_PATH . 'Debug' . DS . 'Toolbar' . DS . 'Views',
-    'max_queries'      => 100,
-    'show_debugbar'    => true,
+    'collectors'          => [],
+    'collect_var_data'    => true,
+    'max_history'         => 20,
+    'view_path'           => SYST_PATH . 'Debug' . DS . 'Toolbar' . DS . 'Views',
+    'max_queries'         => 100,
+    'show_debugbar'       => true,
+    'watched_directories' => ['app'],
+    'watched_extensions'  => ['php', 'css', 'js', 'html', 'svg', 'json', 'env'],
 ];
