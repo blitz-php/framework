0d9e0d05daba35812a1a81882808bf7febc61cb4
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2026-02-07 15:00:34 +0100

    refactor: simplification de l'application des arguments dans BaseMiddleware

--- src/Middlewares/BaseMiddleware.php
+++ src/Middlewares/BaseMiddleware.php
@@ -35,19 +35,27 @@ abstract class BaseMiddleware
         $this->path = $path;
 
         foreach ($this->arguments as $argument => $value) {
-            if (! is_string($argument)) {
-                continue;
-            }
-
-            $method = Text::camel('set_' . $argument);
-            if (method_exists($this, $method)) {
-                call_user_func([$this, $method], $value);
-            } elseif (property_exists($this, $argument)) {
-                $this->{$argument} = $value;
-            }
+            $this->applyArgument($argument, $value);
         }
 
         return $this;
+    }
+
+    /**
+     * Applique un argument au middleware
+     */
+    protected function applyArgument($argument, $value): void
+    {
+        if (! is_string($argument)) {
+            return;
+        }
+
+        $method = Text::camel('set_' . $argument);
+        if (method_exists($this, $method)) {
+            call_user_func([$this, $method], $value);
+        } elseif (property_exists($this, $argument)) {
+            $this->{$argument} = $value;
+        }
     }
 
     public static function make(...$args): static
@@ -65,15 +73,70 @@ abstract class BaseMiddleware
      */
     final public function fill(array $params): static
     {
+        // Si c'est une liste (paramètres positionnels)
+        if (array_values($params) === $params) {
+            $this->fillPositional($params);
+        } else {
+            // Paramètres nommés
+            $this->fillNamed($params);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remplit avec des paramètres positionnels
+     */
+    protected function fillPositional(array $params): void
+    {
         foreach ($this->fillable as $key) {
             if ($params === []) {
                 break;
             }
             $this->arguments[$key] = array_shift($params);
+            $this->applyArgument($key, $this->arguments[$key]);
         }
 
-        $this->arguments += $params;
+        // Paramètres supplémentaires
+        foreach ($params as $index => $value) {
+            $this->arguments[$index] = $value;
+        }
+    }
 
-        return $this;
+    /**
+     * Remplit avec des paramètres nommés
+     */
+    protected function fillNamed(array $params): void
+    {
+        // D'abord, traiter les fillable
+        foreach ($this->fillable as $key) {
+            if (array_key_exists($key, $params)) {
+                $this->arguments[$key] = $params[$key];
+                $this->applyArgument($key, $params[$key]);
+                unset($params[$key]);
+            }
+        }
+
+        // Ensuite, les autres paramètres (pour compatibilité)
+        foreach ($params as $key => $value) {
+            $this->arguments[$key] = $value;
+            $this->applyArgument($key, $value);
+        }
+    }
+
+    /**
+     * Vérifie si un argument existe
+     */
+    public function has($name): bool
+    {
+        return array_key_exists($name, $this->arguments);
+    }
+
+    /**
+     * Récupère tous les arguments
+     */
+    public function all(): array
+    {
+        return $this->arguments;
     }
 }
